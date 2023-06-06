efc0fed135fea44e0b577f30fa9689e21fb31f1d
Melanieislas <120453099+Melanieislas@users.noreply.github.com>
2023-06-05 22:10:01 -0700

    added footer with connected links to contact me

--- melaniei-portfolio/src/App.js
+++ melaniei-portfolio/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 //import NavBar from './components/Navigation';
 
 function App() {
   return(
-    <div>
-      <Header />
-    </div>
+    <>
+      <div className='page-container'>
+        <div className='content-wrap'>
+          <Header />
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ melaniei-portfolio/src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BsGithub, BsLinkedin, BsTwitter} from 'react-icons/bs';
+import '../components/Footer.css'
+
+function Footer() {
+    return (
+        <div className='main-footer'>
+            <div className='container'>
+                <div className='row'>
+                    <h4 className='reach-me'>Reach Me!</h4>
+                    {/*column 1*/}
+                    <a className='col' href="https://github.com/Melanieislas">
+                        <h2><BsGithub /></h2>
+                    </a>
+                    <a className='col' href="https://www.indeed.com/">
+                        <h2 className='col'>< BsLinkedin/></h2>
+                    </a>
+                    <a className='col' href="https://twitter.com/">
+                        <h2 className='col'><BsTwitter /></h2>
+                    </a>
+                </div>
+            </div>
+        </div>
+      );
+}
+
+export default Footer;
