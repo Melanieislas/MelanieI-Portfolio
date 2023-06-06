a01078f22e451af654d23a8e1e4343549419c129
Melanieislas <120453099+Melanieislas@users.noreply.github.com>
2023-06-06 12:40:50 -0700

    added links to deployed projects on project component

--- melaniei-portfolio/src/components/Project.js
+++ melaniei-portfolio/src/components/Project.js
@@ -23,23 +23,29 @@ function Project() {
                                 <div className='titles'>Daily Horoscope <BsGithub /> </div>
                             </a>
                         </div>
-                        <img className='gifs' alt='Horoscope Chart' src={horoscope}></img>
+                        <a href='https://izbeloro.github.io/Daily-Horoscope/'>
+                            <img className='gifs' alt='Horoscope Chart' src={horoscope}></img>
+                        </a>
                     </div>
                     <div className='col'>
                         <div className='row'>
                             <a href="https://github.com/Radioactive-mtb/Buzzer">
                                 <div className='titles'>Buzzer <BsGithub /> </div>
                             </a>
-                        </div>                        
-                        <img className='gifs' alt='Twitter Logo' src={buzzer}></img>
+                        </div>           
+                        <a href='https://fathomless-wave-31109.herokuapp.com/'>             
+                            <img className='gifs' alt='Twitter Logo' src={buzzer}></img>
+                        </a>
                     </div>
                     <div className='col'>
                         <div className='row'>
                             <a href="https://github.com/Radioactive-mtb/highlighter">
                                 <div className='titles'>Highlighter <BsGithub /> </div>
                             </a>
-                        </div>                          
-                        <img className='gifs' alt='Calendar Gif' src={highlighter}></img>
+                        </div>                        
+                        <a href='https://github.com/Radioactive-mtb/highlighter'>  
+                            <img className='gifs' alt='Calendar Gif' src={highlighter}></img>
+                        </a>
                     </div>
                 </div>
                 <div className='row'>
@@ -48,16 +54,20 @@ function Project() {
                             <a href="https://github.com/Melanieislas/Weather-Forecast">
                                 <div className='titles'>Weather Forecast <BsGithub /> </div>
                             </a>
-                        </div>                          
-                        <img className='gifs' alt='Weather Forecast Gif' src={forecast}></img>
+                        </div>
+                        <a href='https://melanieislas.github.io/Weather-Forecast/'>                          
+                            <img className='gifs' alt='Weather Forecast Gif' src={forecast}></img>
+                        </a>
                     </div>
                     <div className='col'>
                         <div className='row'>
                             <a href="https://github.com/Melanieislas/Password_Generator">
                                 <div className='titles'>Password Generator <BsGithub /> </div>
                             </a>
-                        </div>                          
-                        <img className='gifs' alt='Password Generator Gif' src={password}></img>
+                        </div>                  
+                        <a href='https://melanieislas.github.io/Password_Generator/'>        
+                            <img className='gifs' alt='Password Generator Gif' src={password}></img>
+                        </a>
                     </div>
                     <div className='col'>
                         <div className='row'>
@@ -65,7 +75,9 @@ function Project() {
                                 <div className='titles'>Javascript Quiz <BsGithub /> </div>
                             </a>
                         </div>                          
-                        <img className='gifs' alt='Javascript build' src={js}></img>
+                        <a href='https://melanieislas.github.io/Javascript_Quiz/'>
+                            <img className='gifs' alt='Javascript build' src={js}></img>
+                        </a>
                     </div>
                 </div>
             </div>
