a4d23fc67e9023fa6346b38a674cc7dbde0a7c13
Melanieislas <120453099+Melanieislas@users.noreply.github.com>
2023-06-06 21:26:41 -0700

    added my resume to the resume component just need to add a downloadable resume

--- melaniei-portfolio/src/App.js
+++ melaniei-portfolio/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Project from './components/Project';
 import Contact from './components/Contact';
+import Resume from './components/Resume';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AboutMe from './components/AboutMe';
 
@@ -13,13 +14,12 @@ function App() {
       <div className='page-container'>
         <div className='content-wrap'>
           <Header />
-          {/*<NavBar />*/}
         </div>
       <Routes>
         <Route path='/about-me' element={<AboutMe/>} />
         <Route path='/portfolio' element={<Project/>} />
         <Route path='/contact' element={<Contact/>} />
-        <Route path='/resume' element={<Project/>} />
+        <Route path='/resume' element={<Resume/>} />
       </Routes>
       </div>
       <Footer />
---
+++ melaniei-portfolio/src/components/Resume.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import '../components/Resume.css';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+function Resume() {
+    return (
+        <>
+         <Card style={{ width: '80%', 
+        paddingBottom: '10px',
+        margin: '80px',
+         }}>
+      <Card.Body>
+        <Card.Title style={{fontSize: '40px', textAlign: 'center', fontFamily: 'Times New Roman'}}>Resume</Card.Title>
+        <Card.Text style={{textAlign: 'center', fontFamily: 'Times New Roman'}}>
+          <button>Click Here to Download My Resume</button>
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush" style={{fontSize: '20px', fontFamily: 'Times New Roman'}}>
+        <ListGroup.Item style={{fontSize: '30px'}}>Front-end Proficiencies</ListGroup.Item>
+        <ListGroup.Item>HTML</ListGroup.Item>
+        <ListGroup.Item>CSS</ListGroup.Item>
+        <ListGroup.Item>JavaScript</ListGroup.Item>
+        <ListGroup.Item>Responsive Design</ListGroup.Item>
+        <ListGroup.Item>React</ListGroup.Item>
+        <ListGroup.Item>Bootstrap</ListGroup.Item>
+      </ListGroup>
+      <ListGroup className="list-group-flush" style={{fontSize: '20px', fontFamily: 'Times New Roman'}}>
+        <ListGroup.Item style={{fontSize: '30px'}}>Back-end Proficiencies</ListGroup.Item>
+        <ListGroup.Item>APIs</ListGroup.Item>
+        <ListGroup.Item>Node</ListGroup.Item>
+        <ListGroup.Item>Express</ListGroup.Item>
+        <ListGroup.Item>MySQL, Sequelize</ListGroup.Item>
+        <ListGroup.Item>MongoDB, Mongoose</ListGroup.Item>
+        <ListGroup.Item>REST</ListGroup.Item>
+        <ListGroup.Item>GraphQL</ListGroup.Item>
+      </ListGroup>
+    </Card>
+
+        </>
+      );
+}
+
+export default Resume;
