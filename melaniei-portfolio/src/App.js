ac905603c3492750261776081161a896058ef76e
Melanieislas <120453099+Melanieislas@users.noreply.github.com>
2023-06-07 21:14:40 -0700

    added routes

--- melaniei-portfolio/src/App.js
+++ melaniei-portfolio/src/App.js
@@ -10,16 +10,16 @@ import AboutMe from './components/AboutMe';
 
 function App() {
   return(
-    <Router>
+    <Router basename='melaniei-portfolio'>
       <div className='page-container'>
         <div className='content-wrap'>
           <Header />
         </div>
       <Routes>
-        <Route path='/MelanieI-Portfolio' element={<AboutMe/>} />
-        <Route path='/MelanieI-Portfolio/portfolio' element={<Project/>} />
-        <Route path='/MelanieI-Portfolio/contact' element={<Contact/>} />
-        <Route path='/MelanieI-Portfolio/resume' element={<Resume/>} />
+        <Route path='/' element={<AboutMe/>} />
+        <Route path='/portfolio' element={<Project/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/resume' element={<Resume/>} />
       </Routes>
       </div>
       <Footer />
--- melaniei-portfolio/src/index.js
+++ melaniei-portfolio/src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+//import { BrowserRouter as Router} from "react-router-dom";
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+/*
+ReactDOM.render(
+    <Router basename='Melaniei-Portfolio'>
+      <App />
+    </Router>,
+    document.getElementById("root")
+  );
+
+*/
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
